232f4a02be0de226b8b084004375dfba4d20bc35
Jeff Berhow <jberhow@uci.edu>
2016-07-03 07:53:39 -0700

    Added Swarm class and tested

--- swarm.py
+++ swarm.py
@@ -1,3 +1,11 @@
 class Robot():
     def __init__(self,pos):
         self.pos=pos
+
+class Swarm():
+    def __init__(self,swarm_list={}):
+        self.swarm_list=swarm_list
+
+bot1 = Robot((50,50))
+swarm = Swarm()
+
