9ab3328c88d8bfad85bada54dca287f939ff425b
Jeff Berhow <jberhow@uci.edu>
2016-07-03 08:37:07 -0700

    added list_bots function

--- swarm.py
+++ swarm.py
@@ -8,8 +8,17 @@ class Swarm():
     def add_bot(self, robot):
         if robot not in self.swarm_map:
             self.swarm_map[robot]=[]
+    def list_bots(self):
+        for bot in self.swarm_map:
+            print(bot)
 
 bot1 = Robot((50,50))
+bot2 = Robot((50,50))
+bot3 = Robot((50,50))
 swarm = Swarm()
 
 swarm.add_bot(bot1)
+swarm.add_bot(bot2)
+swarm.add_bot(bot3)
+
+swarm.list_bots()
