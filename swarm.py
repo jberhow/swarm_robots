e1e50ee2ea950b74e58d2447a51ec70950af223e
Jeff Berhow <jberhow@uci.edu>
2016-07-03 08:01:46 -0700

    added a function add_bot that adds a robot to the swarm

--- swarm.py
+++ swarm.py
@@ -3,9 +3,13 @@ class Robot():
         self.pos=pos
 
 class Swarm():
-    def __init__(self,swarm_list={}):
-        self.swarm_list=swarm_list
+    def __init__(self,swarm_map={}):
+        self.swarm_map=swarm_map
+    def add_bot(self, robot):
+        if robot not in self.swarm_map:
+            self.swarm_map[robot]=[]
 
 bot1 = Robot((50,50))
 swarm = Swarm()
 
+swarm.add_bot(bot1)
