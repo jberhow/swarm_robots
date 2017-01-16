c12749efdc8b1844565156614cb593244b7f6993
Jeff Berhow <jberhow@uci.edu>
2017-01-16 11:49:29 -0800

    added rotation for robot to mimic real ones

--- simulation/sim.py
+++ simulation/sim.py
@@ -6,6 +6,7 @@
 import pygame
 import sys
 import time
+import math
 
 pygame.init()
 
@@ -13,6 +14,7 @@ size = width, height = 640, 480
 
 colors = {
         'black': (0,0,0),
+        'white': (255,255,255),
         'red': (255,0,0),
         'green': (0,255,0),
         'blue': (0,0,255)
@@ -23,28 +25,44 @@ screen = pygame.display.set_mode(size)
 velocity = velocityX, velocityY = 1, 1
 
 # TODO: move robot class out of sim.py into its own module
+# TODO: see how to make the code more elegant, it's getting rowdy
 class Robot():
 
-    def __init__(self, pos, velx, vely, size=(5, 5), color=colors['green']):
+    def __init__(self, pos, velx, vely, avel, size=(10, 10), color=colors['green']):
         self.pos = pos
         self.size = size
         self.color = color
         self.velx = velx
         self.vely = vely
-        self.rect = pygame.draw.rect(screen, self.color, (self.pos, self.size))
+        self.avel = avel
+        self.rect = pygame.draw.rect(screen, 
+                self.color, (self.pos, self.size))
+
+        self.hangulation = math.pi/2
+        self.direction = pygame.draw.line(screen,
+                colors['white'],self.rect.center, (
+                    self.rect.center[0] + 20*math.cos(self.hangulation), 
+                    self.rect.center[1] - 20*math.sin(self.hangulation)), 2)
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
+        #pygame.draw.rect(screen, colors['white'], self.direction)
+        self.direction = pygame.draw.line(screen,
+                colors['white'],self.rect.center, (
+                    self.rect.center[0] + 20*math.cos(self.hangulation), 
+                    self.rect.center[1] - 20*math.sin(self.hangulation)), 2)
 
     def move(self):
         if (self.rect.x + self.rect.w > width or self.rect.x < 0):
             self.velx = -self.velx
         if (self.rect.y + self.rect.h > height or self.rect.y < 0):
             self.vely = -self.vely
+        self.hangulation = self.hangulation + self.avel
         self.rect.move_ip(self.velx,self.vely)
+        self.direction.move_ip(self.velx,self.vely)
         
-robot1 = Robot((50, 50), 1, 1)
-robot2 = Robot((10, 10), 2, 2)
+robot1 = Robot((50, 50), 1, 1, math.pi/12)
+robot2 = Robot((10, 10), 2, 2, math.pi/24)
 
 robots = [robot1, robot2]
 
