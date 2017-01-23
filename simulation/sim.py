b4f2ea447da37c18fd27e91d70cb901a1d281d7e
Anurag Vaddi Reddy <anurag2surf@gmail.com>
2017-01-22 16:20:04 -0800

    Added obstacle and ir sensor classes. Each robot has 3 ir sensors. When an ir sensor detects an obstacles it will rotate away from the obstacle and move forward. Changed movement and rotation to work independently of delays. Keeps track of how much time has passed since the last move. Added force rotational and translational movement that move the robot regardless of controller input. They are currently used for moving when the ir sensor detects an obstacle.

--- simulation/sim.py
+++ simulation/sim.py
@@ -7,6 +7,8 @@ import pygame
 import sys
 import time
 import math
+import operator
+
 
 pygame.init()
 pygame.key.set_repeat(20,20)
@@ -22,8 +24,8 @@ colors = {
         }
 
 screen = pygame.display.set_mode(size)
-
 velocity = velocityX, velocityY = 1, 1
+automaticMode = False
 
 class Controller():
 
@@ -85,61 +87,206 @@ class Robot():
 
         self.hangulation = math.pi/2
         self.direction = pygame.draw.line(screen,
-                colors['white'],self.rect.center, (
+                colors['blue'],self.rect.center, (
                     self.rect.center[0] + 20*math.cos(self.hangulation), 
                     self.rect.center[1] - 20*math.sin(self.hangulation)), 2)
+        self.sensors = []
+        self.sensors.append(IR_Sensor(tuple(map(operator.add, self.rect.center,
+                                                (self.rect.height/2*math.cos(self.hangulation),
+                                                 -self.rect.height/2*math.sin(self.hangulation)))),
+                                      -(60 * (math.pi / 180))))
+
+        self.sensors.append(IR_Sensor(tuple(map(operator.add, self.rect.center,
+                                                (self.rect.height / 2 * math.cos(self.hangulation),
+                                                 -self.rect.height / 2 * math.sin(self.hangulation)))),
+                                      0))
+
+        self.sensors.append(IR_Sensor(tuple(map(operator.add, self.rect.center,
+                                                (self.rect.height / 2 * math.cos(self.hangulation),
+                                                 -self.rect.height / 2 * math.sin(self.hangulation)))),
+                                      (60 * (math.pi / 180))))
+
+        self.currTimeX = time.time()
+        self.currTimeY = time.time()
+        self.currTimeA = time.time()
+        self.forcedRotationalDifference = 0
+        self.forcedTranslationalDifference = 0
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
-        #pygame.draw.rect(screen, colors['white'], self.direction)
+
+        for sensor in self.sensors:
+            sensor.draw()
+
         self.direction = pygame.draw.line(screen,
-                colors['white'],self.rect.center, (
+                colors['blue'],self.rect.center, (
                     self.rect.center[0] + 20*math.cos(self.hangulation), 
                     self.rect.center[1] - 20*math.sin(self.hangulation)), 
                 2)
 
+
     def update(self):
+        print("Forced Rotational " + str(self.forcedRotationalDifference))
+        print("Forced Translational " + str(self.forcedTranslationalDifference))
         if (self.hangulation >= math.pi):
             self.hangulation = -math.pi
         if (self.hangulation < -math.pi):
             self.hangulation = math.pi
 
-        rotationalDifference = self.hangulation - controller.hangulation
+        if (not self.sensors[0].obstacleDetected and not self.sensors[1].obstacleDetected
+                and not self.sensors[2].obstacleDetected):
+            if(self.forcedRotationalDifference == 0):
+                rotationalDifference = self.hangulation - controller.hangulation
+            else:
+                rotationalDifference = self.forcedRotationalDifference
+        else:
+            if(self.sensors[0].obstacleDetected and self.sensors[1].obstacleDetected):
+                self.forcedRotationalDifference = -25 * (math.pi / 180)
+                self.forcedTranslationalDifference = 50
+            elif (self.sensors[1].obstacleDetected):
+                self.forcedRotationalDifference = 25 * (math.pi / 180)
+                self.forcedTranslationalDifference = 50
+            elif (self.sensors[2].obstacleDetected and self.sensors[1].obstacleDetected):
+                self.forcedRotationalDifference = 25 * (math.pi / 180)
+                self.forcedTranslationalDifference = 50
+            elif(self.sensors[0].obstacleDetected or self.sensors[2].obstacleDetected):
+                rotationalDifference = 0
 
-        if(rotationalDifference == 0):
-            return
-        if(rotationalDifference > 0 and rotationalDifference < math.pi):
-            self.rotate_cw()
-        elif(rotationalDifference > 0 and rotationalDifference >= math.pi):
-            self.rotate_ccw()
-        elif(rotationalDifference < 0 and rotationalDifference > -math.pi):
-            self.rotate_ccw()
-        elif(rotationalDifference < 0 and rotationalDifference <= -math.pi):
-            self.rotate_cw()
+            rotationalDifference = self.forcedRotationalDifference
 
+         #   return
+        if(abs(rotationalDifference) < math.pi/360 or (self.forcedTranslationalDifference > 0 and self.forcedRotationalDifference == 0)):
+            self.forcedRotationalDifference = 0
+            self.currTimeA = time.time()
+            if (pygame.key.get_pressed()[pygame.K_UP] or automaticMode):
+                self.move_forward()
+            elif (pygame.key.get_pressed()[pygame.K_DOWN]):
+                self.move_backward()
+            else:
+                self.currTimeX = time.time()
+                self.currTimeY = time.time()
+        else:
+            self.currTimeX = time.time()
+            self.currTimeY = time.time()
+            if(rotationalDifference > 0 and rotationalDifference < math.pi):
+                self.rotate_cw()
+            elif(rotationalDifference > 0 and rotationalDifference >= math.pi):
+                self.rotate_ccw()
+            elif(rotationalDifference < 0 and rotationalDifference > -math.pi):
+                self.rotate_ccw()
+            elif(rotationalDifference < 0 and rotationalDifference <= -math.pi):
+                self.rotate_cw()
+
+
+        for sensor in self.sensors:
+            sensor.update(tuple(map(operator.add, self.rect.center, (self.rect.height/2*math.cos(self.hangulation),-self.rect.height/2*math.sin(self.hangulation)))), self.hangulation)
 
     def rotate_ccw(self):
-        self.hangulation = self.hangulation + self.avel
+        self.hangulation = self.hangulation + (self.avel * (time.time() - self.currTimeA))
+        if(self.forcedRotationalDifference < 0):
+            self.forcedRotationalDifference += (self.avel * (time.time() - self.currTimeA))
+            if(self.forcedRotationalDifference > 0):
+                self.forcedRotationalDifference = 0
+        self.currTimeA = time.time()
 
     def rotate_cw(self):
-        self.hangulation = self.hangulation - self.avel
+        self.hangulation = self.hangulation - (self.avel * (time.time() - self.currTimeA))
+        if (self.forcedRotationalDifference > 0):
+            self.forcedRotationalDifference -= (self.avel * (time.time() - self.currTimeA))
+            if (self.forcedRotationalDifference < 0):
+                self.forcedRotationalDifference = 0
+        self.currTimeA = time.time()
 
     def move_backward(self):
-        self.rect.move_ip(-self.vel*math.cos(self.hangulation),
-                self.vel*math.sin(self.hangulation))
-        self.direction.move_ip(-self.vel*math.cos(self.hangulation),
-                self.vel*math.sin(self.hangulation))
+        if (abs(self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation)) > 1):
+            self.rect.move_ip(-self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation), 0)
+            self.direction.move_ip(-self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation), 0)
+            self.currTimeX = time.time()
+
+        if (abs(self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation)) > 1):
+            self.rect.move_ip(0, self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation))
+            self.direction.move_ip(0, self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation))
+            self.currTimeY = time.time()
+
     def move_forward(self):
-        self.rect.move_ip(self.vel*math.cos(self.hangulation),
-                -self.vel*math.sin(self.hangulation))
-        self.direction.move_ip(self.vel*math.cos(self.hangulation),
-                -self.vel*math.sin(self.hangulation))
+        if (abs(self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation)) > 1):
+            self.rect.move_ip(self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation), 0)
+            self.direction.move_ip(self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation), 0)
+            if (self.forcedTranslationalDifference > 0):
+                self.forcedTranslationalDifference -= abs(self.vel * (time.time() - self.currTimeX) * math.cos(self.hangulation))
+                if (self.forcedTranslationalDifference < 0):
+                    self.forcedTranslationalDifference = 0
+            self.currTimeX = time.time()
+
+        if (abs(self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation)) > 1):
+            self.rect.move_ip(0, -self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation))
+            self.direction.move_ip(0, -self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation))
+            if (self.forcedTranslationalDifference > 0):
+                self.forcedTranslationalDifference -= abs(self.vel * (time.time() - self.currTimeY) * math.sin(self.hangulation))
+                if (self.forcedTranslationalDifference < 0):
+                    self.forcedTranslationalDifference = 0
+            self.currTimeY = time.time()
+
+class IR_Sensor():
+
+    def __init__(self, position, initialDirection, topLeft=(-4,-20), topRight=(4,-20), bottom=(0, 0), color=colors['white']):
+        self.color = color
+        self.position = position
+        self.initialDirection = initialDirection
+        self.direction = initialDirection
+        self.originalPoints = []
+        self.originalPoints.append(topLeft)
+        self.originalPoints.append(topRight)
+        self.originalPoints.append(bottom)
+        outerPosition = tuple(map(operator.add, position, (20 * math.cos(initialDirection), -20 * math.sin(initialDirection))))
+        self.points = []
+        self.points.append(tuple(map(operator.add, (-4*math.sin(initialDirection), -4*math.cos(initialDirection)), outerPosition)))
+        self.points.append(tuple(map(operator.add, (4*math.sin(initialDirection), 4*math.cos(initialDirection)), outerPosition)))
+        self.points.append(tuple(map(operator.add, bottom, position)))
+        self.rect = pygame.draw.polygon(screen, color, (self.points[0], self.points[1], self.points[2]))
+        self.obstacleDetected = False
+
+    def update(self, position, direction):
+        self.position = position
+        self.direction = direction
+        outerPosition = tuple(map(operator.add, position, (20*math.cos(direction+self.initialDirection),-20*math.sin(direction+self.initialDirection))))
+        points = []
+        points.append(tuple(map(operator.add, (-4*math.sin(direction+self.initialDirection), -4*math.cos(direction+self.initialDirection)), outerPosition)))
+        points.append(tuple(map(operator.add, (4*math.sin(direction+self.initialDirection), 4*math.cos(direction+self.initialDirection)), outerPosition)))
+        points.append(tuple(map(operator.add, self.originalPoints[2], position)))
+        self.points = points
+        if(self.rect.colliderect(obstacles[0].rect)):
+            self.color = colors['red']
+            self.obstacleDetected = True
+        else:
+            self.color = colors['white']
+            self.obstacleDetected = False
+
+    def draw(self):
+        self.rect = pygame.draw.polygon(screen, self.color, (self.points[0], self.points[1], self.points[2]))
+
+class Obstacle():
+
+    def __init__(self, size, pos, color):
+        self.size = size
+        self.pos = pos
+        self.color = color
+        self.rect = pygame.draw.rect(screen,
+                                     self.color, (self.pos, self.size))
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
         
 controller = Controller()
 robots = []
+obstacles = []
+obstacles.append(Obstacle((10, 200), (200, 200), colors['green']))
+
 
 # TODO: controls need to be handled in update()
 def update():
+    global automaticMode
+
     # close out if quit button is pressed on window
     if (pygame.event.peek(pygame.QUIT)):
         pygame.display.quit()
@@ -149,24 +296,25 @@ def update():
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             robots.append(Robot((pygame.mouse.get_pos()[0],
-                pygame.mouse.get_pos()[1]),5,math.pi/180, controller))
+                pygame.mouse.get_pos()[1]),45,math.pi/2, controller))
         if event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_LEFT:
-                controller.rotate_ccw() 
-                for robot in robots:
-                    robot.rotate_ccw() 
+                controller.rotate_ccw()
+
             if event.key == pygame.K_RIGHT:
-                controller.rotate_cw() 
-                for robot in robots:
-                    robot.rotate_cw() 
+                controller.rotate_cw()
+
             if event.key == pygame.K_DOWN:
                 controller.move_backward() 
-                for robot in robots:
-                    robot.move_backward() 
+
             if event.key == pygame.K_UP:
                 controller.move_forward() 
-                for robot in robots:
-                    robot.move_forward()
+
+            if event.key == pygame.K_a:
+                automaticMode = True
+            if event.key == pygame.K_m:
+                automaticMode = False
+
         if event.type == pygame.KEYUP:
             controller.color = colors['white']
 
@@ -176,32 +324,17 @@ def update():
 
 def render():
     screen.fill(colors['black'])
-    font = pygame.font.SysFont("monospace", 15)
-    label = font.render(str(controller.hangulation), 1, (255, 255, 255))
-    screen.blit(label, (100, 100))
+
     controller.draw()
     for robot in robots:
         robot.draw()
+    for obstacle in obstacles:
+        obstacle.draw()
     pygame.display.flip()
-    pygame.time.delay(20)
 
 
 # TODO: maybe make a Game object that gets instantiated in __main__
 while 1:
-
-    # controls
-    # up, down, rotate left, rotate right
-    #for event in pygame.event.get():
-    #    if event.type == pygame.KEYDOWN:
-    #        if event.key == pygame.K_UP:
-    #           robot.move_forward() 
-    #        if event.key == pygame.K_DOWN:
-    #            robot.move_backward()
-    #        if event.key == pygame.K_LEFT:
-    #           robot.rotate_ccw() 
-    #        if event.key == pygame.K_RIGHT:
-    #            robot.rotate_cw()
-
     # update portion
     update()
         
